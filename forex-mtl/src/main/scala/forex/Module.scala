a23780959747f5dbe50d23dabb47b028eab3bdd4
Ford Nutthawat <iamzford@gmail.com>
2025-08-04 13:47:07 +0700

    feat: Add error middleware

--- forex-mtl/src/main/scala/forex/Module.scala
+++ forex-mtl/src/main/scala/forex/Module.scala
@@ -2,36 +2,24 @@ package forex
 
 import cats.effect.{ Concurrent, Timer }
 import forex.config.ApplicationConfig
+import forex.http.middleware.ErrorHandlerMiddleware
 import forex.http.rates.RatesHttpRoutes
 import forex.services._
 import forex.programs._
 import org.http4s._
-import org.http4s.implicits._
 import org.http4s.server.middleware.{ AutoSlash, Timeout }
 
 class Module[F[_]: Concurrent: Timer](config: ApplicationConfig) {
 
-  private val ratesService: RatesService[F] = RatesServices.dummy[F]
-
-  private val ratesProgram: RatesProgram[F] = RatesProgram[F](ratesService)
-
+  private val ratesService: RatesService[F]  = RatesServices.dummy[F]
+  private val ratesProgram: RatesProgram[F]  = RatesProgram[F](ratesService)
   private val ratesHttpRoutes: HttpRoutes[F] = new RatesHttpRoutes[F](ratesProgram).routes
 
-  type PartialMiddleware = HttpRoutes[F] => HttpRoutes[F]
-  type TotalMiddleware   = HttpApp[F] => HttpApp[F]
+  private val allRoutes: HttpRoutes[F] = ratesHttpRoutes
 
-  private val routesMiddleware: PartialMiddleware = {
-    { http: HttpRoutes[F] =>
-      AutoSlash(http)
-    }
-  }
+  private val routesMiddleware: HttpRoutes[F] = AutoSlash(ErrorHandlerMiddleware(allRoutes))
 
-  private val appMiddleware: TotalMiddleware = { http: HttpApp[F] =>
-    Timeout(config.http.timeout)(http)
-  }
+  private val appMiddleware: HttpApp[F] = Timeout(config.http.timeout)(routesMiddleware.orNotFound)
 
-  private val http: HttpRoutes[F] = ratesHttpRoutes
-
-  val httpApp: HttpApp[F] = appMiddleware(routesMiddleware(http).orNotFound)
-
+  val httpApp: HttpApp[F] = appMiddleware
 }
