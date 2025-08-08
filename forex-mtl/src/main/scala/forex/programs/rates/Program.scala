6b5d3d746bcd3022d31176ed57d75afbd111626d
Ford Nutthawat <iamzford@gmail.com>
2025-08-09 00:59:39 +0700

    feat: Add CacheService

--- forex-mtl/src/main/scala/forex/programs/rates/Program.scala
+++ forex-mtl/src/main/scala/forex/programs/rates/Program.scala
@@ -6,17 +6,15 @@ import errors._
 import forex.domain.rates.Rate
 import forex.services.RatesService
 
-class Program[F[_]: Functor](
-    ratesService: RatesService[F]
-) extends Algebra[F] {
+class Program[F[_]: Functor](ratesService: RatesService[F]) extends Algebra[F] {
 
   override def get(request: Protocol.GetRatesRequest): F[RateProgramError Either Rate] =
     EitherT(ratesService.get(Rate.Pair(request.from, request.to)))
       .leftMap(toProgramError)
       .value
+
 }
 
 object Program {
-  def apply[F[_]: Functor](ratesService: RatesService[F]): Algebra[F] =
-    new Program[F](ratesService)
+  def apply[F[_]: Functor](ratesService: RatesService[F]): Algebra[F] = new Program[F](ratesService)
 }
